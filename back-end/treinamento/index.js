b1066251d0432344d7511f1761c65178361ce0de
Marina Oliveira <marinaoliveira.r07@gmail.com>
2020-09-08 22:19:23 -0300

    funcionalidade: habilidade de criar, obter, remover, atualizar posts adiconadas.

--- back-end/treinamento/index.js
+++ back-end/treinamento/index.js
@@ -1,6 +1,7 @@
 
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
+const { COPYFILE_EXCL } = require('constants');
 
 const server = new Koa(); 
 
@@ -48,13 +49,8 @@ server.use(ctx => {
                         },
                     };
                 } else {
-                    posts.splice(i, 1);
-                    console.log(posts);
-                }
-            })
-            autores.forEach(item => {
-                if (item.id === id) {
-                    
+                    autores.splice(i, 1);
+                    console.log(autores);
                 }
             })
         } else {
@@ -64,13 +60,102 @@ server.use(ctx => {
             autores.forEach(item => {
                 if (item.id = id) {
                     item.primeiroNome = ctx.request.body.primeiroNome;
-                    item.ultimomeNome = ctx.request.body.ultimoNome;
+                    item.ultimoNome = ctx.request.body.ultimoNome;
                     item.email = ctx.request.body.email;
                     item.senha = ctx.request.body.senha;
                     ctx.body = item;
                 }
             })
             console.log(autores);
+        }
+    } else if (ctx.url.includes("/posts")) {
+        if (ctx.method === "POST") {
+            if (!ctx.request.body.autor) {
+                ctx.status = 400;
+                ctx.body = {
+                    status: "erro",
+                    dados: {
+                      mensagem: "Autor não escolhido!!",
+                    },
+                  };
+            } else {
+                autores.forEach(item => {
+                    if (item.deletado === "false") {
+                        let post = ctx.request.body;
+                        posts.push(post);
+                        ctx.body = post;
+                    } else {
+                        ctx.status = 400;
+                        ctx.body = {
+                            status: "erro",
+                            dados: {
+                            mensagem: "Autor não existe.",
+                            },
+                        };
+                    }
+                })
+            }
+        } else if (ctx.method === "GET") {
+            const url = ctx.url;
+            if (url === "/posts" || url === "/posts/") {
+                let getPosts = [];
+                posts.forEach(item => {
+                    if (item.deletado === "false") {
+                        getPosts.push(item);
+                        ctx.body = getPosts; 
+                    } 
+                })
+            } else if (url.includes("autor=")){
+                let one = url.split("=");
+                let id = one[1];
+                console.log(id);
+                let authorPosts = [];
+                posts.forEach(item => {
+                    if (item.autor === id) {
+                        authorPosts.push(item);
+                        ctx.body = authorPosts;
+                    }
+                })
+            } else {
+                let one = url.split("/");
+                let id = one[2];
+                posts.forEach(item => {
+                    if (item.id === id && item.deletado === "false") {
+                        ctx.body = item;
+                    } else {
+                        ctx.status = 400; // checar status dps
+                        ctx.body = {
+                            status: "erro",
+                            dados: {
+                                mensagem: "Post não existe ou foi deletado.",
+                            },
+                        };
+                    }
+                })
+            }
+        } else if (ctx.method === "DELETE") {
+            const url = ctx.url;
+            let one = url.split("/");
+            let id = one[2];
+            posts.forEach(item => {
+                if (item.id === id) {
+                    item.deletado = "true";
+                    ctx.body = item;
+                }
+            })
+        } else {
+            const url = ctx.url;
+            let one = url.split("/");
+            let id = one[2];
+            posts.forEach(item => {
+                if (item.id = id) {
+                    item.titulo = ctx.request.body.titulo;
+                    item.autor = ctx.request.body.autor;
+                    item.subtitulo = ctx.request.body.subtitulo;
+                    item.publicado = ctx.request.body.publicado;
+                    ctx.body = item;
+                }
+            })
         }
     }
 })
